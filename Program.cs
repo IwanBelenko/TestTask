67f07abd5cd2e54f0a11eda3607cc1ea6f6cbe22
Ivan <ivan_belenko@list.ru>
2022-12-13 16:07:03 +0300

    Method ArrayReduction

--- Program.cs
+++ Program.cs
@@ -12,3 +12,17 @@ void PrintArray(string[] array)
     }
     Console.WriteLine();
 }
+
+void ArrayReduction(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
